f63eda6667526bbb1a03ff9864c393a1553e13eb
Neel Patel <npatel16@confederationcollege.ca>
2021-08-17 20:55:07 -0400

    Make some changes

--- BreadmakerReport/Program.cs
+++ BreadmakerReport/Program.cs
@@ -16,9 +16,19 @@ namespace BreadmakerReport
             Console.WriteLine("Welcome to Bread World");
             var BreadmakerDb = new BreadMakerSqliteContext(dbfile);
             var BMList = BreadmakerDb.Breadmakers
-                // TODO: add LINQ logic ...
-                //       ...
-                .ToList();
+            // TODO: add LINQ logic ...
+            //       ...
+            .Select(p => new
+            {
+                 Reviews = p.Reviews.Count,
+                 Average = (Double)p.Reviews.Average(q => q.stars),
+                 Adjust = ratingAdjustmentService.Adjust((Double)p.Reviews.Average(q => q.stars), (Double)p.Reviews.Count),
+                 Desc = p.title
+            })
+
+            .ToList()
+            .OrderByDescending(ratingAdjustmentService => ratingAdjustmentService.Adjust)
+            .ToList();
 
             Console.WriteLine("[#]  Reviews Average  Adjust    Description");
             for (var j = 0; j < 3; j++)
@@ -26,7 +36,9 @@ namespace BreadmakerReport
                 var i = BMList[j];
                 // TODO: add output
                 // Console.WriteLine( ... );
+                Console.WriteLine($"[{j + 1}] {i.Reviews} {Math.Round(i.Average, 2)} {Math.Round(i.Adjust, 2)} {i.Desc}");
             }
+            Console.ReadLine();
         }
     }
 }
--- RatingAdjustmentService/RatingAdjustmentService.cs
+++ RatingAdjustmentService/RatingAdjustmentService.cs
@@ -7,6 +7,54 @@ namespace RatingAdjustment.Services
      */
     public class RatingAdjustmentService
     {
-        // TODO: Replace this file with the one from Part A!
+        const double MAX_STARS = 5.0;  // Likert scale
+        const double Z = 1.96; // 95% confidence interval
+
+        double _q;
+        double _percent_positive;
+
+        /** Percentage of positive reviews
+         * 
+         * In this case, that means X of 5 ==> percent positive
+         * 
+         * Returns: [0, 1]
+         */
+        void SetPercentPositive(double stars)
+        {
+            _percent_positive = stars / MAX_STARS;
+        }
+
+        /**
+         * Calculate "Q" given the formula in the problem statement
+         */
+        void SetQ(double number_of_ratings)
+        {
+            double n = number_of_ratings;
+            double p = _percent_positive;
+            _q = Z * Math.Sqrt(((p * (1 - p)) + ((Z * Z) / (4 * n))) / n);
+        }
+
+        /** Adjusted lower bound
+         * 
+         * Lower bound of the confidence interval around the star rating.
+         * 
+         * Returns: a double, up to 5
+         */
+        public double Adjust(double stars, double number_of_ratings) {
+            SetPercentPositive(stars);
+            SetQ(number_of_ratings);
+            double in_bound = ((_percent_positive + ((Z * Z) / (2 * number_of_ratings)) - _q) / (1 + ((Z * Z) / number_of_ratings)))*5 ;
+            
+            if(in_bound < MAX_STARS)
+            {
+                return in_bound;
+            }
+            else
+            {
+                return stars;
+            }
+
+           
+        }
     }
 }
